0bd567802d858aff4c0693ed5a0a3fb0e7fa2ecd
Minal Chaudhari <minalchaudhari@promactinfo.com>
2023-06-28 12:07:25 +0530

    Commit Changes

--- DTO/SkillRequestDTO.cs
+++ DTO/SkillRequestDTO.cs
@@ -1,16 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace FirstAngularAPIAssignment.DTO
 {
     public class SkillRequestDTO
-    {        
-        public int SrNo { get; set; }
+    {  
 
         [Required(ErrorMessage = "Please Enter Skill Name.")]
+        [JsonProperty("skillName")]
         public string SkillName { get; set; }
 
         [Required(ErrorMessage = "Please Select Skill Experience.")]
+        [JsonProperty("skillExperience")]
         public float SkillExperience { get; set; }
+        [JsonProperty("employeeId")]
         public int EmployeeId { get; set; }
     }
 }
